1e864ac4799f5ebd6a090467d986a22b40fa7586
Sanusasaas <said0555d@mail.ru>
2025-10-16 01:38:38 +0300

    Load

--- models/cars.go
+++ models/cars.go
@@ -3,13 +3,13 @@ package models
 import "gorm.io/gorm"
 
 type Cars struct {
-	ID uint `gorm:"primary key;atoIncrement" json:"id"`
-	Brand *string `json:"brand"`
-	Name *string `json:"name"`
+	ID    uint     `gorm:"primary key;autoIncrement" json:"id"`
+	Brand *string  `json:"brand"`
+	Name  *string  `json:"name"`
 	Price *float64 `json:"price"`
 }
 
-func MigrateCars(db *gorm.DB) error{
+func MigrateCars(db *gorm.DB) error {
 	err := db.AutoMigrate(&Cars{})
 	return err
 }
