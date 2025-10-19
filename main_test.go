11232b58afec42dbb25b0a72483f5fd18a679947
Sanusasaas <said0555d@mail.ru>
2025-10-20 01:08:18 +0300

    update test

--- main_test.go
+++ main_test.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func ValidateCarCreate(car Car) error{
+func ValidateCarCreate(car Car) error {
 	if car.Price == 0 {
 		return fmt.Errorf("price can't be 0")
 	}
@@ -14,20 +16,22 @@ func ValidateCarCreate(car Car) error{
 
 func TestValidateCarCreate(t *testing.T) {
 	tests := []struct {
-		name string
-		car Car
+		name    string
+		car     Car
 		wantErr bool
 	}{
-		{"Valid price", Car{Brand: "Toyota", Name: "Supra", Price: 1000}, false},
+		{"Valid price", Car{Brand: "Toyota", Name: "Supra", Price: 10000}, false},
 		{"Invalid price", Car{Brand: "Toyota", Name: "Supra", Price: 0}, true},
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T){
+		t.Run(tt.name, func(t *testing.T) {
 			err := ValidateCarCreate(tt.car)
-			if (err!=nil) != tt.wantErr {
-			t.Errorf("ValidateCarCreate() error = %v, wantErr = %v",err, tt.wantErr)
-		}
+			if tt.wantErr {
+				assert.Error(t, err, "ValidateCarCreate() should return an error")
+			} else {
+				assert.NoError(t, err, "ValidateCarCreate() should not return an error")
+			}
 		})
 	}
 }
