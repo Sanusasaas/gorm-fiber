594cf6e70a1e8d1554b68191b12bbcbcd72f4f64
Sanusasaas <said0555d@mail.ru>
2025-10-19 21:27:06 +0300

    price test

--- main_test.go
+++ main_test.go
@@ -25,8 +25,8 @@ func TestValidateCarCreate(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T){
 			err := ValidateCarCreate(tt.car)
-			if (err!=nil) == tt.wantErr {
-			t.Errorf("ValidateCarCreate() error = %v, wantErr = %v",err,tt.wantErr)
+			if (err!=nil) != tt.wantErr {
+			t.Errorf("ValidateCarCreate() error = %v, wantErr = %v",err, tt.wantErr)
 		}
 		})
 	}
